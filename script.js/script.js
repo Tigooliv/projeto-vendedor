5bd187f8f879ead65e6bc0049dca2b403c27d2f3
Tigooliv <tigoolive1985@gmail.com>
2023-11-25 01:07:40 -0300

    Add files via upload

---
+++ script.js/script.js
@@ -0,0 +1,27 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+let contador = 1;
+
+setInterval(function(){
+    document.getElementById('slide' + contador).checked = true;
+    contador ++;
+
+    if(contador > 3) {
+        contador -1;
+    }
+}, 3000); 
